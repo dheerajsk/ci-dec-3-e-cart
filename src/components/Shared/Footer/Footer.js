5603f96dd6054a9aa7fa51bb02695dd09e1fbe05
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-12-03 14:23:17 +0530

    Header & Footer

---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,7 @@
+import "./Footer.css";
+
+function Footer() {
+  return <footer className="footer">Copyright 2022-2023</footer>;
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import Footer from "../../components/Shared/Footer/Footer";
+import Header from "../../components/Shared/Header/Header";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
